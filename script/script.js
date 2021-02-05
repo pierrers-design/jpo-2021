b510a036e5831497956146cea273de2049b2e284
pierrers-design <74794864+pierrers-design@users.noreply.github.com>
2021-02-05 16:05:43 +0100

    Beta_v.0.2.1
    Corretion de bugs, ajout des liens vers les réunions

--- script/script.js
+++ script/script.js
@@ -153,11 +153,11 @@ var open00 = true;
 $(".question-00").click(function () {
 	if (open00 == true) {
 		$(".question-00").css("height", "10vw");
-		$(".fleche-question-faq-00").css("rotate", "-90deg");
+		$(".fleche-question-faq-00").css("transform", "rotate(-90deg)");
 		open00 = false;
 	} else if (open00 == false) {
 		$(".question-00").css("height", "2.8vw");
-		$(".fleche-question-faq-00").css("rotate", "90deg");
+		$(".fleche-question-faq-00").css("transform", "rotate(90deg)");
 		open00 = true;
 	}
 });
@@ -166,11 +166,11 @@ var open01 = true;
 $(".question-01").click(function () {
 	if (open01 == true) {
 		$(".question-01").css("height", "10vw");
-		$(".fleche-question-faq-01").css("rotate", "-90deg");
+		$(".fleche-question-faq-01").css("transform", "rotate(-90deg)");
 		open01 = false;
 	} else if (open01 == false) {
 		$(".question-01").css("height", "2.8vw");
-		$(".fleche-question-faq-01").css("rotate", "90deg");
+		$(".fleche-question-faq-01").css("transform", "rotate(90deg)");
 		open01 = true;
 	}
 });
@@ -179,11 +179,11 @@ var open02 = true;
 $(".question-02").click(function () {
 	if (open02 == true) {
 		$(".question-02").css("height", "20.5vw");
-		$(".fleche-question-faq-02").css("rotate", "-90deg");
+		$(".fleche-question-faq-02").css("transform", "rotate(-90deg)");
 		open02 = false;
 	} else if (open02 == false) {
 		$(".question-02").css("height", "2.8vw");
-		$(".fleche-question-faq-02").css("rotate", "90deg");
+		$(".fleche-question-faq-02").css("transform", "rotate(90deg)");
 		open02 = true;
 	}
 });
@@ -192,11 +192,11 @@ var open03 = true;
 $(".question-03").click(function () {
 	if (open03 == true) {
 		$(".question-03").css("height", "12vw");
-		$(".fleche-question-faq-03").css("rotate", "-90deg");
+		$(".fleche-question-faq-03").css("transform", "rotate(-90deg)");
 		open03 = false;
 	} else if (open03 == false) {
 		$(".question-03").css("height", "2.8vw");
-		$(".fleche-question-faq-03").css("rotate", "90deg");
+		$(".fleche-question-faq-03").css("transform", "rotate(90deg)");
 		open03 = true;
 	}
 });
@@ -205,11 +205,11 @@ var open04 = true;
 $(".question-04").click(function () {
 	if (open04 == true) {
 		$(".question-04").css("height", "7.5vw");
-		$(".fleche-question-faq-04").css("rotate", "-90deg");
+		$(".fleche-question-faq-04").css("transform", "rotate(-90deg)");
 		open04 = false;
 	} else if (open04 == false) {
 		$(".question-04").css("height", "2.8vw");
-		$(".fleche-question-faq-04").css("rotate", "90deg");
+		$(".fleche-question-faq-04").css("transform", "rotate(90deg)");
 		open04 = true;
 	}
 });
@@ -218,11 +218,11 @@ var open05 = true;
 $(".question-05").click(function () {
 	if (open05 == true) {
 		$(".question-05").css("height", "6vw");
-		$(".fleche-question-faq-05").css("rotate", "-90deg");
+		$(".fleche-question-faq-05").css("transform", "rotate(-90deg)");
 		open05 = false;
 	} else if (open05 == false) {
 		$(".question-05").css("height", "2.8vw");
-		$(".fleche-question-faq-05").css("rotate", "90deg");
+		$(".fleche-question-faq-05").css("transform", "rotate(90deg)");
 		open05 = true;
 	}
 });
@@ -231,11 +231,11 @@ var open06 = true;
 $(".question-06").click(function () {
 	if (open06 == true) {
 		$(".question-06").css("height", "9vw");
-		$(".fleche-question-faq-06").css("rotate", "-90deg");
+		$(".fleche-question-faq-06").css("transform", "rotate(-90deg)");
 		open06 = false;
 	} else if (open06 == false) {
 		$(".question-06").css("height", "2.8vw");
-		$(".fleche-question-faq-06").css("rotate", "90deg");
+		$(".fleche-question-faq-06").css("transform", "rotate(90deg)");
 		open06 = true;
 	}
 });
@@ -244,11 +244,11 @@ var open07 = true;
 $(".question-07").click(function () {
 	if (open07 == true) {
 		$(".question-07").css("height", "6vw");
-		$(".fleche-question-faq-07").css("rotate", "-90deg");
+		$(".fleche-question-faq-07").css("transform", "rotate(-90deg)");
 		open07 = false;
 	} else if (open07 == false) {
 		$(".question-07").css("height", "2.8vw");
-		$(".fleche-question-faq-07").css("rotate", "90deg");
+		$(".fleche-question-faq-07").css("transform", "rotate(90deg)");
 		open07 = true;
 	}
 });
@@ -257,11 +257,11 @@ var open08 = true;
 $(".question-08").click(function () {
 	if (open08 == true) {
 		$(".question-08").css("height", "12vw");
-		$(".fleche-question-faq-08").css("rotate", "-90deg");
+		$(".fleche-question-faq-08").css("transform", "rotate(-90deg)");
 		open08 = false;
 	} else if (open08 == false) {
 		$(".question-08").css("height", "2.8vw");
-		$(".fleche-question-faq-08").css("rotate", "90deg");
+		$(".fleche-question-faq-08").css("transform", "rotate(90deg)");
 		open08 = true;
 	}
 });
@@ -270,11 +270,11 @@ var open09 = true;
 $(".question-09").click(function () {
 	if (open09 == true) {
 		$(".question-09").css("height", "13.5vw");
-		$(".fleche-question-faq-09").css("rotate", "-90deg");
+		$(".fleche-question-faq-09").css("transform", "rotate(-90deg)");
 		open09 = false;
 	} else if (open09 == false) {
 		$(".question-09").css("height", "2.8vw");
-		$(".fleche-question-faq-09").css("rotate", "90deg");
+		$(".fleche-question-faq-09").css("transform", "rotate(90deg)");
 		open09 = true;
 	}
 });
@@ -283,11 +283,11 @@ var open10 = true;
 $(".question-10").click(function () {
 	if (open10 == true) {
 		$(".question-10").css("height", "39vw");
-		$(".fleche-question-faq-10").css("rotate", "-90deg");
+		$(".fleche-question-faq-10").css("transform", "rotate(-90deg)");
 		open10 = false;
 	} else if (open10 == false) {
 		$(".question-10").css("height", "2.8vw");
-		$(".fleche-question-faq-10").css("rotate", "90deg");
+		$(".fleche-question-faq-10").css("transform", "rotate(90deg)");
 		open10 = true;
 	}
 });
@@ -296,11 +296,11 @@ var open11 = true;
 $(".question-11").click(function () {
 	if (open11 == true) {
 		$(".question-11").css("height", "39vw");
-		$(".fleche-question-faq-11").css("rotate", "-90deg");
+		$(".fleche-question-faq-11").css("transform", "rotate(-90deg)");
 		open11 = false;
 	} else if (open11 == false) {
 		$(".question-11").css("height", "2.8vw");
-		$(".fleche-question-faq-11").css("rotate", "90deg");
+		$(".fleche-question-faq-11").css("transform", "rotate(90deg)");
 		open11 = true;
 	}
 });
@@ -309,11 +309,11 @@ var open12 = true;
 $(".question-12").click(function () {
 	if (open12 == true) {
 		$(".question-12").css("height", "39vw");
-		$(".fleche-question-faq-12").css("rotate", "-90deg");
+		$(".fleche-question-faq-12").css("transform", "rotate(-90deg)");
 		open12 = false;
 	} else if (open12 == false) {
 		$(".question-12").css("height", "2.8vw");
-		$(".fleche-question-faq-12").css("rotate", "90deg");
+		$(".fleche-question-faq-12").css("transform", "rotate(90deg)");
 		open12 = true;
 	}
 });
@@ -322,11 +322,11 @@ var open13 = true;
 $(".question-13").click(function () {
 	if (open13 == true) {
 		$(".question-13").css("height", "41vw");
-		$(".fleche-question-faq-13").css("rotate", "-90deg");
+		$(".fleche-question-faq-13").css("transform", "rotate(-90deg)");
 		open13 = false;
 	} else if (open13 == false) {
 		$(".question-13").css("height", "2.8vw");
-		$(".fleche-question-faq-13").css("rotate", "90deg");
+		$(".fleche-question-faq-13").css("transform", "rotate(90deg)");
 		open13 = true;
 	}
 });
@@ -335,11 +335,11 @@ var open14 = true;
 $(".question-14").click(function () {
 	if (open14 == true) {
 		$(".question-14").css("height", "10.5vw");
-		$(".fleche-question-faq-14").css("rotate", "-90deg");
+		$(".fleche-question-faq-14").css("transform", "rotate(-90deg)");
 		open14 = false;
 	} else if (open14 == false) {
 		$(".question-14").css("height", "2.8vw");
-		$(".fleche-question-faq-14").css("rotate", "90deg");
+		$(".fleche-question-faq-14").css("transform", "rotate(90deg)");
 		open14 = true;
 	}
 });
@@ -348,25 +348,25 @@ var open15 = true;
 $(".question-15").click(function () {
 	if (open15 == true) {
 		$(".question-15").css("height", "26.5vw");
-		$(".fleche-question-faq-15").css("rotate", "-90deg");
+		$(".fleche-question-faq-15").css("transform", "rotate(-90deg)");
 		open15 = false;
 	} else if (open15 == false) {
 		$(".question-15").css("height", "2.8vw");
-		$(".fleche-question-faq-15").css("rotate", "90deg");
+		$(".fleche-question-faq-15").css("transform", "rotate(90deg)");
 		open15 = true;
 	}
 });
 
-var openindex = true;
+var opendsaadnmade = true;
 $(".conteneur-dsaa-dnmade").click(function () {
-	if (openindex == true) {
+	if (opendsaadnmade == true) {
 		$(".conteneur-dsaa-dnmade").css("height", "11vw");
-		$(".fleche-text-faq").css("rotate", "-90deg");
-		openindex = false;
-	} else if (openindex == false) {
+		$(".fleche-text-faq").css("transform", "rotate(-90deg)");
+		opendsaadnmade = false;
+	} else if (opendsaadnmade == false) {
 		$(".conteneur-dsaa-dnmade").css("height", "3vw");
-		$(".fleche-text-faq").css("rotate", "90deg");
-		openindex = true;
+		$(".fleche-text-faq").css("transform", "rotate(90deg)");
+		opendsaadnmade = true;
 	}
 });
 
